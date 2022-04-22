4a5bba5d868ca05d5cbaeb89c900a748ae027ccf
Te12944265-AMAHA <1603699457@qq.com>
2022-04-21 22:29:51 -0400

    verified compatibility with ubuntu 18.04

--- camera_model/src/intrinsic_calib.cc
+++ camera_model/src/intrinsic_calib.cc
@@ -1,8 +1,8 @@
 #include <boost/algorithm/string.hpp>
 //#include <boost/filesystem.hpp>
-//#define BOOST_NO_CXX11_SCOPED_ENUMS
+//#define BOOST_NO_SCOPED_ENUMS
 #include <boost/filesystem.hpp>
-//#undef BOOST_NO_CXX11_SCOPED_ENUMS
+//#undef BOOST_NO_SCOPED_ENUMS
 #include <boost/program_options.hpp>
 #include <iomanip>
 #include <iostream>
